aabdcec1dddf6869ff9104ebbbd9bbbd9bd62d6a
Kleity <perezaguirrejazmin@gmail.com>
2020-07-13 20:51:46 -0500

    [UPDATE]

--- app.py
+++ app.py
@@ -75,6 +75,3 @@ def delete():
 port = int(os.environ.get('PORT', 8080))
 if __name__ == '__main__':
     app.run(threaded=True, host='0.0.0.0', port=port)
-
-if __name__ == '__main__':
-    app.run(debug = True, port=8080)
